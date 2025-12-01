a45cf446b2173af34e7673a869f1450cdbd57f42
MALKA <m0556727693@gmail.com>
2025-12-01 17:05:04 +0200

    startStepThree

---
+++ DotNet2025_0784_5297/DalFacade/DalApi/ICustomer.cs
@@ -0,0 +1,14 @@
+﻿using DO;
+namespace DalApi;
+/// <summary>
+/// 
+/// </summary>
+public interface ICustomer
+{
+    int Create(Customer item);
+    Customer? Read(int id);
+    List<Customer> ReadAll();
+    void Update(Customer item);
+    void Delete(int id);
+}
+
